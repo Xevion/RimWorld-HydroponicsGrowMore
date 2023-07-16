97f122e6d0edf353e2178364e2112245b50e35a0
Xevion <xevion@xevion.dev>
2023-07-16 13:44:43 -0500

    Setup build script + .gitignore

---
+++ build.ps1
@@ -0,0 +1,15 @@
+Write-Output "Removing old files."
+
+Remove-Item ./HydroponicsGrowMore/ -Recurse -Force
+
+Write-Output "Copying files."
+
+mkdir ./HydroponicsGrowMore
+mkdir ./HydroponicsGrowMore/About
+Copy-Item ./About/About.xml ./HydroponicsGrowMore/About/About.xml
+Copy-Item ./About/Preview.png ./HydroponicsGrowMore/About/Preview.png
+Copy-Item ./Patches/ ./HydroponicsGrowMore/Patches/ -Recurse
+
+Write-Output "Zipping files."
+
+Compress-Archive -Path ./HydroponicsGrowMore/* -DestinationPath ./HydroponicsGrowMore.zip
